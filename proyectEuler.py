87360bf9dae59138144d413a3d29ee51e4fcefc3
koryroot <20197914@itla.edu.do>
2021-04-13 12:06:03 -0400

    multiplos

---
+++ proyectEuler.py
@@ -0,0 +1,15 @@
+#1
+print("Determine si un número es múltiplo de 3 y 5.")
+def Multiplo(nm):
+    if nm % 3 == 0 & nm % 5 == 0:
+        print("El numero es multiplo de 3 y 5")
+    elif nm % 3 == 0:
+        print("El numero es multiplo de 3")
+    elif nm % 5 == 0:
+        print("El numero es multiplo de 5")
+    else:
+        print("El numero que ha introduccido no es multiplo")
+mn = int(input("Introduce un numero: "))
+Multiplo(mn)
+
+#2
