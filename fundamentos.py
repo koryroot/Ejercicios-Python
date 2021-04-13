c77522c897584e5dd5ea47a4904f8eff6877efc3
koryroot <20197914@itla.edu.do>
2021-04-13 10:51:09 -0400

    funciones

--- fundamentos.py
+++ fundamentos.py
@@ -3,42 +3,45 @@
 ""Análisis: Para la solución de este problema, se requiere que
 ingrese dos números por teclado y el sistema realice el proceso
 de devolver el número mayor."""
-
-print("-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*")
-print("Este programa es para determinar el mayor de dos numeros")  
+def nMayor(num1,num2):
+    print("Este programa es para determinar el mayor de dos numeros")  
+    if n1 > n2:
+        print(f"El numero mayor es: {n1}")
+    else:
+        print(f"El numero mayor es: {n2}")
+    
 n1 = int(input("Ingrese el primer numero: "))
 n2 = int(input("Ingrese el segundo numero: "))
-
-if n1 > n2:
-    print(f"El numero mayor es: {n1}")
-else:
-    print(f"El numero mayor es: {n2}")
-
+nMayor(n1,n2)
 print("-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*")
 
 print("Determinar si un número es positivo, negativo o neutro.")
-"""Para la solución de este problema, se requiere que
-ingrese un número entero por teclado y el sistema verifique si
-es positivo, neutro o negativo."""
+def Num(n):
+    if n1 < 0:
+        print("El numero es negativo")
+    elif n1 > 0:
+        print("El numero es positivo")
+    else:
+        print("El numero es neutro")
 
 n1 = int(input("Introduce un numero: "))
-if n1 < 0:
-    print("El numero es negativo")
-elif n1 > 0:
-    print("El numero es positivo")
-else:
-    print("El numero es neutro")
+Num(n1)
 print("-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*")
 
 #programa para determinar si se ha igresado un numero
+def Idnum(n1)
+    if cosa.isdigit():
+        print("El valor ingresado es un numero" )
+    else:
+        print("Eso no es un numero e.e")
 
 cosa = input("Digita un numero: ")
-if cosa.isdigit():
-    print("El valor ingresado es un numero" )
-else:
-    print("Eso no es un numero e.e")
+Idnum(cosa)
+   
 
 #3. Problema 03: Dado un carácter determine si es una Vocal o no.
 """Análisis: Para la solución de este problema, se requiere que
 usuario ingrese un carácter y el sistema verifique si es un
 vocal o no."""
+
+#print("-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*")
